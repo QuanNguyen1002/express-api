66a0d02fd1f77685224de34d9b5c11d179c086ea
QuanNguyen1002 <quan100201@gmail.com>
2023-09-24 09:10:50 +0700

    fix host

--- swagger-config.js
+++ swagger-config.js
@@ -1,11 +1,11 @@
 // swagger-config.js
-
+const dotenv = require("dotenv").config().parsed;
 const doc = {
     info: {
         title: 'My Express API',
         description: 'Documentation for my Express API',
     },
-    host: process.env.host || 'localhost:3000', // Replace with your server's host and port
+    host: dotenv.API_HOST || 'localhost:3000', // Replace with your server's host and port
     basePath: '/',
 };
 
--- swagger.js
+++ swagger.js
@@ -1,10 +1,8 @@
 // swagger.js
-
 const swaggerAutogen = require('swagger-autogen')();
 const outputFile = './swagger_output.json';
 
 const doc = require('./swagger-config'); // Import the Swagger config from swagger-config.js
-
 swaggerAutogen(outputFile, ['app.js'], doc).then(() => {
     require('./app.js'); // Start your Express app
 });
